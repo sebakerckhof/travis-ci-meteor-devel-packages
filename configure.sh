24836b3d46e1cc2a86aebae840a74cbbbf2b11d6
SEKE <sebastian.kerckhof@barco.com>
2015-10-23 10:03:27 +0200

    update urls

--- configure.sh
+++ configure.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 #configuring the system
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/Makefile
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/start_test.js
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/phantom_runner.js
-
-#install latest meteor
-curl https://install.meteor.com | /bin/sh
+wget https://raw.github.com/sebakerckhof/travis-ci-meteor-devel-packages/master/start_test-devel.js
 
 #clone devel branch
 git clone git://github.com/meteor/meteor.git ~/meteor
